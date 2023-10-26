af6e703d976e98b47835c4ee4d76dcd0b844f480
Carsten Linder <Carsten@Carsten-Linder.de>
2023-10-26 15:18:44 +0200

    Style changes on selection

--- src/components/Answer/Answer.js
+++ src/components/Answer/Answer.js
@@ -1,11 +1,23 @@
 import './Answer.css'
 
-const Answer = () => {
-  return (
-    <button class='answer' data-testid='answer'>
-        <p>Answer</p>
-    </button>
-  )
+const Answer = ({ isCorrect, clickHandler, hasSelected }) => {
+
+
+    return (
+        <>
+        {hasSelected ? 
+            <button className={`answer ${isCorrect}`} data-testid='answer'>
+                <p>Answer true</p>
+            </button> 
+            :
+            <button className={`answer`} data-testid='answer' onClick={clickHandler}>
+                <p>Answer true</p>
+            </button> 
+        }
+        </>
+        )
+    
+  
 }
 
 export default Answer
--- src/components/Answers/Answers.js
+++ src/components/Answers/Answers.js
@@ -1,13 +1,24 @@
 import './Answers.css'
 import Answer from '../Answer/Answer'
+import { useState } from 'react'
 
 const Answers = () => {
+
+    const [ hasSelected, setHasSelected ] = useState(false);
+
+
+    function clickHandler(e) {
+        console.log(e.target.getAttribute("class"))
+        setHasSelected(true)
+        
+    }
+
   return (
     <div data-testid='answers' id='answers'>
-      <Answer />
-      <Answer />
-      <Answer />
-      <Answer />
+      <Answer isCorrect={true} clickHandler={clickHandler} hasSelected={hasSelected}/>
+      <Answer isCorrect={false} clickHandler={clickHandler} hasSelected={hasSelected}/>
+      <Answer isCorrect={false} clickHandler={clickHandler} hasSelected={hasSelected}/>
+      <Answer isCorrect={false} clickHandler={clickHandler} hasSelected={hasSelected}/>
     </div>
   )
 }
