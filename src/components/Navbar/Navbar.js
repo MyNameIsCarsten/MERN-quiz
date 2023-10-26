93981dded7de113838fe787e51292b8793bac8ba
Carsten Linder <Carsten@Carsten-Linder.de>
2023-10-26 13:34:03 +0200

    Basic Layout

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div data-testid='navbar'>
+      <nav>Navbar</nav>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/Progressbar/Progressbar.js
@@ -0,0 +1,11 @@
+import './Progressbar.css';
+
+const Progressbar = () => {
+  return (
+    <div data-testid='progressbar'>
+      <footer>Progressbar</footer>
+    </div>
+  )
+}
+
+export default Progressbar
